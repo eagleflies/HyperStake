9892f7ad648b9b756dc69ab261fc2fdae7a0cce6
Eagle Flies <myeagleflies@gmail.com>
2014-10-27 18:19:18 +0000

    testing

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -209,7 +209,8 @@ void OverviewPage::setBalance(qint64 balance, qint64 stake, qint64 unconfirmedBa
     QString market = "BTC_HYP";
     QString str;
     QString currentRate = OverviewPage::getCurrentRate(market);
-    double currentValue = currentRate.toDouble() * currentBalance; 
+//    double currentValue = currentRate.toDouble() * currentBalance; 
+    double currentValue = 12345678;
     ui->labelValue->setText(BitcoinUnits::formatWithUnit(REAL_BTC_UNIT, currentValue)); 
 
 
